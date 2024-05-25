c6dc41cba38bc7bd644b156e628b516f613dff68
Filip Karnis <karnisfilip@gmail.com>
2024-05-25 18:56:16 +0200

    django: set static files for azure

--- vegetation/urls.py
+++ vegetation/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -40,4 +42,5 @@ urlpatterns = [
     path(API_PREFIX + 'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path(API_PREFIX, include('vegetation_app.urls')),
     path(API_PREFIX, include('vegetation_user_app.urls')),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
